110cbcf1e29df6d71fe06917e84a0069226c385d
denis_savitsky <denis_savickii96@mail.ru>
2020-04-23 14:04:53 +0300

    module 3.6 tasks added

--- Part3 (scripts and problems)/conftest.py
+++ Part3 (scripts and problems)/conftest.py
@@ -1,23 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: Chrome or Firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language: ru, en, etc.')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
     browser = None
     if browser_name == 'Chrome':
         print("\nstart Chrome browser for test..")
-        browser = webdriver.Chrome()
+        # language settings for Chrome
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == 'Firefox':
         print("\nstart Firefox browser for test..")
-        browser = webdriver.Firefox()
+        # language settings for Firefox
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError(f"--browser_name should be Chrome or Firefox, while your input is {browser_name}")
+
     yield browser
     print('\nquit browser...')
     browser.quit()
---
+++ Part3 (scripts and problems)/test_param_task.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pytest
+import time
+import math
+
+@pytest.fixture(scope='function')
+def browser():
+    print('\nstart browser...')
+    browser = webdriver.Chrome()
+    yield browser
+    print('\nquit browser...')
+    browser.quit()
+
+links = ['https://stepik.org/lesson/236895/step/1',
+         'https://stepik.org/lesson/236896/step/1',
+         'https://stepik.org/lesson/236897/step/1',
+         'https://stepik.org/lesson/236898/step/1',
+         'https://stepik.org/lesson/236899/step/1',
+         'https://stepik.org/lesson/236903/step/1',
+         'https://stepik.org/lesson/236904/step/1',
+         'https://stepik.org/lesson/236905/step/1']
+
+@pytest.mark.parametrize('link', links)
+class TestParams():
+
+    def test_parametrization(self, browser,link):
+        browser.get(link)
+        textarea = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'textarea'))
+        )
+        textarea.send_keys(str(math.log(int(time.time()))))
+        button = browser.find_element(By.CSS_SELECTOR, '.submit-submission')
+        button.click()
+        respond = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'smart-hints__hint'))
+        ).text
+        assert respond == 'Correct!'
+
